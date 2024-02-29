123da70eeabdeac8b5a99bf20f2c3a7c5a83f082
carolmt <carolinamaldonado12@hotmail.es>
2024-02-29 15:55:24 +0100

    Ejercicio2

--- src/main/java/DownloaderAndZipper.java
+++ src/main/java/DownloaderAndZipper.java
@@ -1,6 +1,22 @@
 import javafx.collections.ListChangeListener;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+
 public class DownloaderAndZipper implements ListChangeListener<String> {
+
+    private List<Path> archivosDescargados = new ArrayList<>();
+
     @Override
     public void onChanged(Change<? extends String> change) {
         //metodo mientras haya cambios
@@ -10,6 +26,37 @@ public class DownloaderAndZipper implements ListChangeListener<String> {
                 //separar la url del nombre aleatorio
                 String[] parts = change.getAddedSubList().get(0).split(" ");
                 System.out.println(parts[0] + " encolado como " + parts[1]);
+            }
+        }
+    }
+
+    //metodo para descargar el fichero
+    public CompletableFuture<Void>descargarFichero(String url, String nombreFichero){
+        //devuelve un futuro que se ejecutara en un hilo separado
+        return CompletableFuture.runAsync(() -> {
+            try (InputStream in = new URL(url).openStream()) {
+                //creamos el path del fichero
+                Path outputPath = Paths.get(nombreFichero);
+                //copiamos el contenido del fichero en el path
+                Files.copy(in, outputPath);
+                //Añadimos el path a la lista de archivos descargados
+                archivosDescargados.add(outputPath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+    public void comprimirArchivo() throws IOException {
+        //creamos el zip con nombre compressed.zip
+        try (ZipOutputStream zos = new ZipOutputStream(Files.newOutputStream(Paths.get("contenidoUrls.zip")))) {
+
+            //recorremos la lista de archivos descargados para ir añadiendolos al zip
+            for (Path file : archivosDescargados) {
+            //añadimos cada fichero a la compresion
+                ZipEntry zipEntry = new ZipEntry(file.getFileName().toString());
+                zos.putNextEntry(zipEntry);
+                Files.copy(file, zos);
+                zos.closeEntry();
             }
         }
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
 
+/*Amplía la aplicación anterior para que en el momento de que el usuario introduzca una URL vacía, se proceda a descargar
+ todas las URLs cada una con un nombre
+ de fichero (el nombre aleatorio único creado) y al terminar este proceso se comprimirán todos los archivos en un
+  único archivo .ZIP. Esto lo deberás hacer
+ usando Futuros de Java, de modo que la descarga de todos los archivos es la primera parte del futuro y cuando este
+ proceso termina se ejecuta la
+compresión de todos ellos en un archivo .zip.*/
 
 public class Main {
 //creamos una cadena de caracteres aleatoria
@@ -19,6 +30,9 @@ public class Main {
         ObservableList<String> urls = FXCollections.observableArrayList();
         urls.addListener(downloader);
 
+        //lista de futuros para las descargas
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
+
         //bucle infinito para introducir urls
         while (true) {
             System.out.println("Introduce una URL:");
@@ -27,11 +41,21 @@ public class Main {
             //condicion de fin d bucle
             if (url.isEmpty()) {
                 System.out.println("Se va a proceder a descargar y comprimir los ficheros");
+                CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).thenRun(() -> {
+                    try {
+                        downloader.comprimirArchivo();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                        });
                 break;
             }
             //llamos al metodo para generar la clave aleatoria
             String randomString = generarCadenaRandom();
             urls.add(url + " " + randomString);
+            //añadimos a la lista de futuros la descarga del fichero
+            futures.add(downloader.descargarFichero(url, randomString));
+
         }
     }
 
